77eebdbcf0f46f39cfbd2f8ae97a4cfeab7df068
Dylan Theriot <dylantheriot@gmail.com>
2021-01-11 06:26:50 -0600

    allowing users to pick their own word

--- pogchamp.js
+++ pogchamp.js
@@ -9,12 +9,22 @@ chrome.storage.local.get('pogchampion', function(res) {
 });
 
 setInterval(function () { 
-  var x = document.querySelectorAll('.chat-line__message .text-fragment:not(.pogged)'); 
+  var x = document.querySelectorAll('.chat-line__message .text-fragment:not(.pogged)');
+  let pogChampionWord = '';
+  chrome.storage.local.get('word', function(res) {
+    if (res.word) {
+      pogChampionWord = res.word;
+    }
+    else {
+      // default
+      pogChampionWord = 'PogChampion';
+    }
+  }) 
   if (x.length !== 0) { 
     for (let i = 0; i < x.length; i++) {
       x[i].classList.add("pogged")
       chrome.storage.local.get('pogchampion', function(res) {
-        x[i].innerHTML = x[i].innerHTML.replaceAll("PogChamp", `<img class="chat-image chat-line__message--emote" style="max-height: 28px; width: auto" src=${res.pogchampion} alt="PogChamp">`);
+        x[i].innerHTML = x[i].innerHTML.replaceAll(pogChampionWord, `<img class="chat-image chat-line__message--emote" style="max-height: 28px; width: auto" src=${res.pogchampion} alt="PogChamp">`);
       });
     }
   }; 
